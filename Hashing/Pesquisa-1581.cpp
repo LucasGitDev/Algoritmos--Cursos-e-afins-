36625f2020a63eba40efbe80915cd1777789ba1b
LucasGitDev <ls5651405@gmail.com>
2021-09-17 20:05:46 -0300

    init

---
+++ Hashing/Pesquisa-1581.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+
+using namespace std;
+
+//1581 URI
+//Algoritmo de busca usando técnica de HASH
+
+struct dado
+{
+    int k;      // chave
+    int status; // 0:vazio / 1:ocupado
+};
+
+int hash_aux(int k, int m)
+{
+    return (k % m);
+}
+
+int h(int k, int i, int m)
+{
+    return ((hash_aux(k, m) + i) % m);
+}
+// Função de inserção hash de acordo com exemplo
+int hash_insert(dado t[], int m, int k)
+{
+    int i;
+    int j;
+
+    i = 0;
+
+    do
+    {
+        j = h(k, i, m);
+        if (t[j].status != 1)
+        {
+            t[j].k = k;
+            t[j].status = 1;
+            return j;
+        }
+        else
+            i += 1;
+    } while (i != m);
+
+    return -1;
+}
+
+int hash_search(dado t[], int m, int k)
+{
+    int i;
+    int j;
+    i = 0;
+
+    do
+    {
+        j = h(k, i, m);
+        if (t[j].k == k)
+            return j;
+        i += 1;
+    } while (t[j].status != 0 && i < m);
+
+    return -1;
+}
+
+int main()
+{
+    int value;          // valor a inserir
+    int n;              // tamanho do vetor
+    dado table[100];    // tabela de valores
+    int i;              // contador
+    int resultOfSearch; // Resultado da busca pelo elemento
+    int elem;           //Elemento à ser pesquisado
+    cin >> n;
+
+    // iniciando tabela
+    for (i = 0; i < n; i++)
+    {
+        table[i].k = -1;
+        table[i].status = 0;
+    }
+
+    // Ler valores e inserir usando função
+    cin >> value;
+    while (value != 0)
+    {
+        hash_insert(table, n, value);
+        cin >> value;
+    }
+
+    // Pesquisar elemento
+    cin >> elem;
+
+    resultOfSearch = hash_search(table, n, elem);
+
+    // Show result of search
+
+    if (resultOfSearch == -1)
+        cout << "Chave " << elem << " nao encontrada\n";
+    else
+        cout << "Chave " << elem << " encontrada na posicao " << resultOfSearch << endl;
+
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Algoritmos--Cursos-e-afins-
