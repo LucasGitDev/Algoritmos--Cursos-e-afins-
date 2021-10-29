b3388850b685cb82ed48665b3d0cd390838bd59a
lucaspds <lucas@pdsolucoes.com.br>
2021-10-29 20:54:06 -0300

    add grafos com peso e orientado

--- Grafos/grafos-exemplo1.cpp
+++ Grafos/grafos-exemplo1.cpp
@@ -13,17 +13,32 @@ struct no
 int main() {
 
     list<no> adj[10]; // lista de adjacência
-    int nVertices; // número de vértices
+    int nVertices = 4; // número de vértices
     int u, v; // origem e destino da aresta
     int p; // peso da aresta
     int i; // contador
     no aux; // auxiliar para inserir arestas em adj
+    list<no>::iterator q; // iterador para percorrer lista de adjacência
 
-    for(i = 0; i < 4; i++) {
+    //Lendo e inserindo aresta
+    for(i = 0; i < nVertices; i++) {
         cin >> u >> v >> p;
         aux.v = v;
         aux.peso = p;
         adj[u].push_back(aux);
+    }
+
+    //Mostrando a lista de adjacência
+    for(i = 0; i < nVertices; i++) {
+        for(q = adj[i].begin(); q != adj[i].end(); q++) {
+            cout << i << "(" << q->v << ", " << q->peso << ") ";
+        }
+        cout << endl;
+    }
+
+    //limpar a memória
+    for(i = 0; i < nVertices; i++) {
+        adj[i].clear();
     }
 
     return 0;
